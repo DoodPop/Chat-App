7e507d8e942118a73ac99fc31ed5d38323bb0381
DoodPop <hypergon101@gmail.com>
2024-05-22 18:16:27 -0600

    work

--- login.ts
+++ login.ts
@@ -1,37 +1,33 @@
 function oauthSignIn() {
-  // Google's OAuth 2.0 endpoint for requesting an access token
   var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
   var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('method', 'GET');
   form.setAttribute('action', oauth2Endpoint);
 
-  // Parameters to pass to OAuth 2.0 endpoint.
   var params = {
-    'client_id': '3574196466-ck8gl2id3cv2dolhkrtnpdp1cbi5m4hq.apps.googleusercontent.com',
-    'redirect_uri': 'https://chat-app-tawny-seven.vercel.app/chat.html',
-    'response_type': 'token',
-    'scope': 'profile email',
-    'include_granted_scopes': 'true',
-    'state': 'pass-through value'
+      'client_id': '3574196466-ck8gl2id3cv2dolhkrtnpdp1cbi5m4hq.apps.googleusercontent.com',
+      'redirect_uri': 'https://chat-app-tawny-seven.vercel.app/chat.html',
+      'response_type': 'token',
+      'scope': 'profile email',
+      'include_granted_scopes': 'true',
+      'state': 'pass-through value'
   };
 
-  // Add form parameters as hidden input values.
   for (var p in params) {
-    if (params.hasOwnProperty(p)) {
-      var input = document.createElement('input');
-      input.setAttribute('type', 'hidden');
-      input.setAttribute('name', p);
-      input.setAttribute('value', params[p]);
-      form.appendChild(input);
-    }
+      if (params.hasOwnProperty(p)) {
+          var input = document.createElement('input');
+          input.setAttribute('type', 'hidden');
+          input.setAttribute('name', p);
+          input.setAttribute('value', params[p]);
+          form.appendChild(input);
+      }
   }
 
-  // Add form to page and submit it to open the OAuth 2.0 endpoint.
   document.body.appendChild(form);
   form.submit();
 }
+
 function fetchUserProfile(token) {
   console.log('Access Token:', token);
 
@@ -42,9 +38,7 @@ function fetchUserProfile(token) {
       })
       .then(function (data) {
           console.log('User Profile:', data);
-          // Store user data in local storage
           localStorage.setItem('userData', JSON.stringify(data));
-          // Redirect to chat page after storing the data
           window.location.href = 'chat.html';
       })
       .catch(function (error) {
@@ -61,8 +55,7 @@ function handleOAuthResponse() {
   }, {});
 
   if (result.access_token) {
-      var token = result.access_token;
-      fetchUserProfile(token);
+      fetchUserProfile(result.access_token);
   } else {
       console.error('Access token not found.');
   }
