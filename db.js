90c2e7ec2122b398170e57d0e7a3ff88454236d6
DoodPop <hypergon101@gmail.com>
2024-06-10 16:23:46 -0600

    lolol

--- db.js
+++ db.js
@@ -1,40 +1,36 @@
-const { Pool, Client } = require('pg');
-
-const config = {
-  user: 'avnadmin',
-  host: 'pg-3e7f2846-postgresqldb.g.aivencloud.com',
-  database: 'chatapp',
-  password: 'AVNS_C4x7Sfu9yg1PN9RxchX', 
-  port: 15264,
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var pg_1 = require("pg");
+var config = {
+    user: 'avnadmin',
+    host: 'pg-3e7f2846-postgresqldb.g.aivencloud.com',
+    database: 'chatapp',
+    password: 'AVNS_C4x7Sfu9yg1PN9RxchX',
+    port: 15264,
 };
-
-const client = new Client(config);
-
+var client = new pg_1.Client(config);
 client.connect()
-  .then(() => {
+    .then(function () {
     console.log('Connected to the database');
-
-    const userData = {
-      email: 'user@example.com', 
-      name: 'John Doe', 
+    var userData = {
+        email: 'user@example.com',
+        name: 'John Doe',
     };
-
-    const query = {
+    var query = {
         text: 'INSERT INTO users(user_name, user_gmail) VALUES($1, $2)',
         values: [userData.name, userData.email],
-      };
-
+    };
     client.query(query)
-      .then(() => {
+        .then(function () {
         console.log('User information inserted into the database');
-      })
-      .catch((err) => {
+    })
+        .catch(function (err) {
         console.error('Error inserting user information into the database:', err);
-      })
-      .finally(() => {
-        client.end(); 
-      });
-  })
-  .catch((err) => {
+    })
+        .finally(function () {
+        client.end();
+    });
+})
+    .catch(function (err) {
     console.error('Error connecting to the database:', err);
-  });
+});
