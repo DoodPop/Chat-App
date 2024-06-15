48ee62c7dfe8bee8f23fe655b754b48abca38eff
DoodPop <hypergon101@gmail.com>
2024-06-15 14:24:04 -0600

    axiosfix

--- userinfo.js
+++ userinfo.js
@@ -1,12 +1,13 @@
-var axios = require('axios');
+
 var userData = {
     name: 'John Doe',
     email: 'john@example.com',
 };
+
 axios.post('https://servs.vercel.app', userData)
     .then(function (response) {
-    console.log('User data pushed successfully:', response.Data);
-})
+        console.log('User data pushed successfully:', response.data);
+    })
     .catch(function (error) {
-    console.error('Error pushing user data:', error);
-});
+        console.error('Error pushing user data:', error);
+    });
